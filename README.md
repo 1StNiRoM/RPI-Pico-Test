2579dcb49fe20f10392c974507e6ed0b7b6216fe
blake <blake.r.mccallum@gmail.com>
2024-08-02 21:43:48 +1200

    GPIO comunication serial

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # RPI-Pico-Test
 
 This is a just me teating the RPI Pico with PlatformIO.
+
+Usage
+With a serial monitor send a 32bit binnary message formated as so.
+XXXXXYYYZZZZZZZZ
+
+X being the pin
+Y being the operation
+Z being the value (some operations)
+
+current operations are 0 for digitalWrite
+1 for digitalRead(not functional)
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,85 @@
-#include <Arduino.h>
+#include <Arduino.h> // Including the arduino library
 
-
-// Example 1 - Receiving single characters
-
-int receivedChar;
+// boolean to check for new data
 boolean newData = false;
 
-void recvOneChar();
-void showNewData();
+// IDK what this does but it works
+typedef void (*functionsArray) (int pin, int value);
 
+// Function prototypes
+void recvCommand();
+void showNewData();
+void runCommand();
+void writeDigital(int pin, int value);
+void readDigital(int pin, int value);
+
+// Command structure
+struct {
+   unsigned int type : 3; // 3 bits for command type (9 values)
+   unsigned int pin : 5;  // 5 bits for pin number (32 values)
+   byte value;            // 8 bits for pin value (256 values)
+} command;
+
+// Create array of functions
+functionsArray functions[] = 
+  {
+      writeDigital, 
+      readDigital
+  };
+
+// Setup, starts serial
 void setup() {
   Serial.begin(9600);
   Serial.println("<Arduino is ready>");
 }
 
+// Loop, loops
 void loop() {
-  recvOneChar();
-  showNewData();
+  recvCommand();  // Gets a new command from serial 
+  runCommand();   // Runs that command
 }
 
-void recvOneChar() {
+// Checks if new data is avalible then publishes it to command structure
+void recvCommand() {
   if (Serial.available() > 0) {
-    receivedChar = Serial.read();
+    byte b1 = Serial.read();
+    byte b2 = Serial.read();
+    
+    command.type = b1;
+    command.pin = b1 >> 3;
+    command.value = b2;
     newData = true;
   }
 }
 
+// Prints the data - For debuging
 void showNewData() {
   if (newData == true) {
-    Serial.print("This just in ... ");
-    Serial.println(receivedChar);
+    Serial.print("Type: ");
+    Serial.println(command.type);
+    Serial.print("Pin: ");
+    Serial.println(command.pin);
+    Serial.print("Value: ");
+    Serial.println(command.value);
+  }
+}
+
+// Runs the commands
+void runCommand() {
+  if (newData == true) {
+    showNewData();   // Print for debuging
+    functions[command.type](command.pin, command.value);
     newData = false;
   }
 }
+
+// The functions
+
+void writeDigital(int pin, int value) {
+  digitalWrite(pin, value);
+}
+
+void readDigital(int pin, int value) {
+  Serial.println(digitalRead(pin));
+}
+
